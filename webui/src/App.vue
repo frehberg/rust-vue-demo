43ed9b1310fa0c753e6923bff36bbc84cc5f2a07
frehberg <frehberg@gmail.com>
2022-12-18 23:33:12 +0100

    tailwindcss style

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <div id="app">
-    <h2>Data Monitor</h2>
-    {{ service_url }}
-    <br />
-    Message Counter: {{ counter }}
-    <br />
-    <button v-on:click="sendMessage('hello')">Send Hello</button>
-    <br />
-    <p style="white-space: pre-line">
-    <br />
-    </p>
-    <div>
-     <dl class="list-group list-group-flush text-left">
-        <dl class="list-group-item" v-for="message in messages" v-bind:key="message" >
-           <span >
-              {{message}}
-           </span>
-        </dl>
-     </dl>
+
+  <div id="app" class="h-screen">
+    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Data Monitor</h1>
+    <div class="my-2">
+       URL: {{ service_url }}
+    </div>
+    <div class="h-0.5 bg-gray-200 w-36 mx-auto mt-2.5"></div>
+
+    <form>
+      <div class="flex justify-center items-center py-2">
+        <input class="appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="123#DEADBEEF" aria-label="frame">
+        <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+          Send Frame
+        </button>
+      </div>
+    </form>
+
+    <div class="border">
+      <ul>
+        <li v-for="message in messages" v-bind:key="message">
+          {{ message }}
+        </li>
+      </ul>
+
     </div>
   </div>
 </template>
@@ -26,40 +31,47 @@
 
 export default {
   name: 'App',
-  data: function() {
+  data: function () {
     return {
       messages: [],
       connection: null,
       counter: 0,
       service_url: "unknown",
+      frame: "123#DEADBEEF",
     }
   },
   methods: {
-    sendMessage: function(message) {
-      console.log("Hello")
-      console.log(this.connection);
-      this.connection.send(message);
+    sendMessage: function (frame) {
+      console.log("Send CAN frame", frame)
+      this.connection.send(frame);
+      const zeroPadHex = (num, places) => String(num.toString(16)).padStart(places, '0');
+
+      if (this.messages.length > 20) {
+        this.messages.shift();
+      }
+      this.messages.push("<--  " + zeroPadHex(this.counter, 8) + ": " + frame);
     }
   },
 
-  created: function() {
+
+  created: function () {
     let vm = this
     console.log("Starting connection to WebSocket Server")
     this.connection = new WebSocket(((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/ws");
-    this.connection.onmessage = function(event) {
+    this.connection.onmessage = function (event) {
       const zeroPadHex = (num, places) => String(num.toString(16)).padStart(places, '0');
-      
+
       console.log(event);
       if (vm.messages.length > 20) {
-        vm.messages.shift(); 
+        vm.messages.shift();
       }
       let parsedMessage = JSON.parse(event.data);
       vm.counter += 1;
       vm.service_url = parsedMessage.service_url;
-      vm.messages.push(zeroPadHex(parsedMessage.counter, 8) + ": " + parsedMessage.body)
+      vm.messages.push("------>  " + zeroPadHex(vm.counter, 8) + ": " + parsedMessage.body)
     }
 
-    this.connection.onopen = function(event) {
+    this.connection.onopen = function (event) {
       console.log(event)
       console.log("Successfully connected to the echo websocket server...")
     }
@@ -76,6 +88,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.message {
+  float: left
 }
 </style>
 
